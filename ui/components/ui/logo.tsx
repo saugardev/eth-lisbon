56f0984f5b4028a815ff4b8ad994f381210285e8
Saúl García Céspedes <saul@dixwe.com>
2023-05-13 15:51:47 +0100

    chore: remove unnecesary stuff

--- ui/components/ui/logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function Logo() {
-  return (
-    <Link href="/" className="text-2xl font-extrabold tracking-wider text-gray-700" aria-label="Cruip">
-      Guvn000r
-    </Link>
-  )
-}
